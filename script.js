223df1d108b6bb49a0ea84880f16923d91207b1b
Scott Thoreson <scott.thoreson@live.com>
2022-01-11 12:16:40 -0500

    Finished chapter 25 exercises.

---
+++ script.js
@@ -0,0 +1,10 @@
+const postSimulation = document.getElementById("runSim");
+const inputList = document.getElementsByTagName("form").children;
+console.log(inputList.length);
+postSimulation.addEventListener("click",function(){
+    // let inputs = [];
+    console.log(inputList);
+    if(inputList. document.hasChildNodes()){
+        console.log(document.children);
+    }//.getElementsByTagName("form");
+})
